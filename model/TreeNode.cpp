4c8df5c8eb809cc7bf52164548f0329e141c7a35
lance1313 <jlin3312@740s-pl-102.local>
2016-04-11 12:26:01 -0600

    Finished constructor creation,in cpp

--- model/TreeNode.cpp
+++ model/TreeNode.cpp
@@ -6,6 +6,39 @@
 //  Copyright © 2016 CTEC. All rights reserved.
 //
 
+#include "Node.h"
 #include "TreeNode.hpp"
 
-#include "Node.h"
+namespace CTECData
+{
+    template <class Type>
+    TreeNode<Type> :: TreeNode() : Node<Type>()
+    {
+        this->leftChild = nullptr;
+        this->righthild = nullptr;
+        this->parent = nullptr;
+    }
+    
+    template <class Type>
+    TreeNode<Type> :: TreeNode(const Type& value) : Node<Type>(value)
+    {
+        this->leftChild = nullptr;
+        this->righthild = nullptr;
+        this->parent = nullptr;
+        
+        this->setValue(value);
+        
+    }
+    
+    template <class Type>
+    TreeNode<Type> :: TreeNode(const Type& value, TreeNode<Type> * parent) : Node<Type>(value)
+    {
+        this->leftChild = nullptr;
+        this->righthild = nullptr;
+        this->parent = parent;
+    }
+    
+}
+
+
+
--- model/TreeNode.hpp
+++ model/TreeNode.hpp
@@ -11,4 +11,35 @@
 
 #include <stdio.h>
 
+#include "Node.h"
+#include "TreeNode.hpp"
+
+namespace CTECData
+{
+    template <class Type>
+    class TreeNode : public Node<Type>
+    {
+    private:
+        TreeNode<Type> * leftChild;
+        TreeNode<Type> * rightChild;
+        TreeNode<Type> * parent;
+        
+    public:
+        TreeNode<Type>();
+        TreeNode<Type>(const Type& value);
+        TreeNode<Type>(const Type& value, TreeNode<Type> * parent);
+        void setLeftChild(TreeNode<Type> * leftChild);
+        void setRightChild(TreeNode<Type> * rightChild);
+        void setParent(TreeNode<Type> * parent);
+        
+        TreeNode<Type> * geLeftChild();
+        TreeNode<Type> * getRightChild();
+        TreeNode<Type> * getParent();
+        
+    };
+    
+    
+}
+
+
 #endif /* TreeNode_hpp */
