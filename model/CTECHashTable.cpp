b0e0c89b5a00cb282405df929a12c4e6cc11befd
lance1313 <jlin3312@740s-pl-102.local>
2016-05-09 12:00:30 -0600

    Made HasNode class to frame the Hash table projetc

--- model/CTECHashTable.cpp
+++ model/CTECHashTable.cpp
@@ -19,19 +19,63 @@ template <class Type>
     this->size = 0;
     this->capacity = 101;
     this->efficiencyPercentage = .667;
-    this->internalStora = new Type[capacity];
+    this->internalStorage = new Type[capacity];
     
 }
-
+/*
+ *deallocates all memory attched to the internal strogare.
+ *
+ */
 template <class Type>
 CTECHashTable<Type>:: ~CTECHashTable()
 {
     delete [] internalStorage;
 }
 
+/*
+*return the size of the structure
+*
+ */
+ 
+ 
 template <class Type>
 int CTECHashTable<Type>:: getSize()
 {
     return this->size;
 }
+
+/*
+ *Check if the val;ue exists in the table ifo
+ *it will update the size if needed then find the place
+ *for the item in the table. Then loop
+ */
+ 
+template <class Type>
+void CTECHashTable<Type>:: add(const Type& value)
+{
+    if(!contains(value))
+    {
+        //resize if needed.
+        if(size/capacity >= this->efficiencyPercentage)
+        {
+            updateSize();
+        }
+        int insertionIndex = findPosition(value);
+        
+        if(internalStorage[insertionIndex] != nullptr)
+        {
+            while(internalStorage[insertionIndex] != nullptr)
+            {
+                insertionIndex = (insertionIndex + 1) % capacity;
+            }
+        }
+        internalStorage[insertionIndex] = value;
+        size++;
+    }
+}
+
+
+
+
+
 
---
+++ model/HashNode.cpp
@@ -0,0 +1,9 @@
+//
+//  HashNode.cpp
+//  NewNodes
+//
+//  Created by Lindquist, Jacob on 5/9/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "HashNode.hpp"
